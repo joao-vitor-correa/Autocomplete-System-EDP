145e43d3a9d99442190e5bdfb9624a677c4aa326
joao <jvjoaoandrades@gmail.com>
2025-12-07 22:32:18 -0300

    UPDATE: servindo arquivos do front pelo servidor e adicionando menu de nacvegacao

--- server.c
+++ server.c
@@ -20,21 +20,50 @@
 
 #define PORT 8080
 
+void send_file(SOCKET client, const char *filepath)
+{
+    FILE *file = fopen(filepath, "rb");
+    if (!file)
+    {
+        const char *err =
+            "HTTP/1.1 404 Not Found\r\n"
+            "Content-Type: text/html\r\n"
+            "Access-Control-Allow-Origin: *\r\n"
+            "Connection: close\r\n"
+            "\r\n"
+            "<h1>404 - Arquivo não encontrado</h1>";
+
+        send(client, err, (int)strlen(err), 0);
+        return;
+    }
+
+    const char *header =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/html\r\n"
+        "Access-Control-Allow-Origin: *\r\n"
+        "Connection: close\r\n"
+        "\r\n";
+
+    send(client, header, (int)strlen(header), 0);
+
+    char buffer[1024];
+    size_t bytes;
+
+    while ((bytes = fread(buffer, 1, sizeof(buffer), file)) > 0)
+    {
+        send(client, buffer, (int)bytes, 0);
+    }
+
+    fclose(file);
+}
+
 int main()
 {
     SOCKET server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
     socklen_t addrlen = sizeof(address);
-    char buffer[1024] = {0};
-
-    const char *html_response =
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/html\r\n"
-        "Access-Control-Allow-Origin: *\r\n"
-        "Connection: close\r\n"
-        "\r\n"
-        "<h1>Servidor HTTP funcionando!</h1>";
+    char buffer[2048];
 
     const char *ping_response =
         "HTTP/1.1 200 OK\r\n"
@@ -48,7 +77,7 @@ int main()
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
     {
-        fprintf(stderr, "Falha ao inicializar Winsock\n");
+        printf("Erro ao inicializar Winsock\n");
         return 1;
     }
 #endif
@@ -60,25 +89,21 @@ int main()
         return 1;
     }
 
-#ifndef _WIN32
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-#endif
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt));
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    if (SOCKET_ERROR_CHECK(bind(server_fd, (struct sockaddr *)&address, sizeof(address))))
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
     {
         perror("Erro no bind");
-        CLOSESOCKET(server_fd);
         return 1;
     }
 
-    if (SOCKET_ERROR_CHECK(listen(server_fd, 10)))
+    if (listen(server_fd, 10) < 0)
     {
         perror("Erro no listen");
-        CLOSESOCKET(server_fd);
         return 1;
     }
 
@@ -90,21 +115,40 @@ int main()
 
         new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
         if (SOCKET_ERROR_CHECK(new_socket))
+            continue;
+
+        int received = recv(new_socket, buffer, sizeof(buffer) - 1, 0);
+        if (received <= 0)
         {
-            perror("Erro no accept");
+            CLOSESOCKET(new_socket);
             continue;
         }
 
-        recv(new_socket, buffer, sizeof(buffer) - 1, 0);
-        printf("Requisição recebida:\n%s\n", buffer);
+        printf("Requisição:\n%s\n", buffer);
 
         if (strncmp(buffer, "GET /ping", 9) == 0)
         {
-            send(new_socket, ping_response, strlen(ping_response), 0);
+            send(new_socket, ping_response, (int)strlen(ping_response), 0);
+        }
+        else if (strncmp(buffer, "GET / HTTP", 10) == 0)
+        {
+            send_file(new_socket, "Front-end/index.html");
+        }
+        else if (strncmp(buffer, "GET /test", 9) == 0)
+        {
+            send_file(new_socket, "Front-end/test.html");
+        }
+        else if (strncmp(buffer, "GET /chat", 9) == 0)
+        {
+            send_file(new_socket, "Front-end/chat.html");
+        }
+        else if (strncmp(buffer, "GET /fruits", 11) == 0)
+        {
+            send_file(new_socket, "Front-end/fruits.html");
         }
         else
         {
-            send(new_socket, html_response, strlen(html_response), 0);
+            send_file(new_socket, "Front-end/index.html");
         }
 
         CLOSESOCKET(new_socket);
