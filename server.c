c4d51db7f225d10b6ea7a8a1040e36bde615cf2f
joao <jvjoaoandrades@gmail.com>
2025-12-08 20:52:20 -0300

    ADD: adicionando dados para carrega_dados

--- server.c
+++ server.c
@@ -30,17 +30,96 @@ NoAVL *raiz = NULL;
 
 void carregar_dados()
 {
-    const char *lista[] = {
-        "abacate", "abacaxi", "abrico", "acai", "acerola", "amora", "pera",
-        "ameixa", "araca", "araticum", "bacaba", "bacuri", "banana",
-        "buriti", "caja", "caju", "camucamu", "carambola", "ciriguela",
-        "cupuaçu", "damasco", "figo",
-        "framboesa-do-mato", "goiaba", "graviola", "groselha",
-        "jabuticaba", "jaca", "jambo", "jenipapo", "jiricó",
-        "laranja-lima", "limao-cravo", "mamao",
-        "manga", "maracuja", "murici", "umbu", "uvaia",
-        "pitanga", "pitaya-brasileira", "pequi", "pinha",
-        "seriguela", "tamarindo", "tucumã", "uva-do-japao"};
+    const char *lista[] = {"abacate", "abacaxi", "abiu", "açaí", "acerola", "acelora-do-mato", "ackee", "african-cucumber",
+                           "aguai", "agresta", "aguaviva", "alfarroba", "ameixa", "ameixa-amarela", "ameixa-de-natal",
+                           "ameixa-japonesa", "ameixa-roxa", "amêndoa", "amora", "amora-branca", "amora-preta", "amora-vermelha",
+                           "araticum", "araticum-do-brejo", "araucária-fruto", "araticum-cagão", "abiu-grande",
+                           "ata", "ata-brava", "atemoia", "avelã",
+
+                           "banana", "banana-maçã", "banana-nanica", "banana-prata", "banana-da-terra", "banana-ouro",
+                           "bacaba", "bacabinha", "bacuri", "bacupari", "bambu-fruta", "baru", "bergamota",
+                           "bergamota-murro", "biribá", "bilberry", "black-sapote", "blueberry", "boysenberry",
+                           "buriti", "butiá", "butiá-do-cerrado",
+
+                           "cabeludinha", "cacau", "cagaita", "cagaitinha", "cajá", "cajá-manga", "cajá-umbu", "caju",
+                           "calabura", "camapu", "cambuci", "camu-camu", "caqui", "carambola", "carnaúba-fruto",
+                           "castanha-portuguesa", "castanha-do-pará", "cereja", "cereja-do-rio-grande",
+                           "cereja-de-pitanga", "cereja-europeia", "cereja-negra", "cereja-amarela",
+                           "cirigüela", "coco", "coco-verde", "coco-anão",
+                           "cranberry", "cupuaçu", "curuba", "curriola",
+
+                           "damasco", "damasco-negro", "damasco-siberiano", "dekopon", "desert-lime",
+                           "doce-de-abelha-fruto", "dragon-fruit", "durião",
+
+                           "embaúba-fruto", "embaubarana", "embaúba-do-brejo", "embiriba",
+                           "enset", "esfregadinha-fruto", "etrog",
+
+                           "figo", "figo-da-índia", "figo-missioneiro", "framboesa", "framboesa-amarela",
+                           "fruta-do-conde", "fruta-pão", "fruta-de-lobo", "fruta-de-ema", "fruta-de-sabiá",
+                           "fruta-de-macaco", "fruta-de-tatu", "fruta-milagrosa", "fruta-pinha",
+
+                           "gabi-fruto", "gabiroba", "gabiroba-do-campo", "gaiaku", "gamboa-fruto", "genipapo",
+                           "goiaba", "goiaba-vermelha", "goiaba-branca", "golden-berry", "grapefruit", "graviola",
+                           "groselha", "groselha-negra", "groselha-vermelha", "guabiroba", "guabiju", "guapuruvu-fruto",
+                           "guaraná", "guarana-do-mato", "guavião",
+
+                           "habañero-fruit-pepper", "hackberry", "halim-fruto", "hawthorn-berry", "honeyberry",
+                           "hurmá (caqui oriental)",
+
+                           "ilama", "ingá", "ingá-de-metro", "ingá-do-brejo", "ingá-mirim", "ixi-pixi",
+
+                           "jabuticaba", "jabuticaba-sabará", "jabuticaba-branca", "jabuticaba-açu",
+                           "jaca", "jaca-mole", "jaca-dura", "jambo", "jambo-vermelho", "jambo-amarelo",
+                           "jamelão", "jenipapo", "jerivá-fruto", "jiripoca-fruto", "juá", "jujuba",
+                           "juçara (fruto da palmeira)",
+
+                           "kiwano", "kiwi", "kiwi-amarelo", "kumquat",
+
+                           "laranja", "laranja-pera", "laranja-baía", "laranja-lima", "laranja-seleta",
+                           "laranja-sanguínea", "lichia", "limão", "limão-siciliano", "limão-taiti", "limequat",
+                           "longan", "loquat (nêspera)", "lulo", "lucuma",
+
+                           "maçã", "maçã-verde", "maçã-gala", "maçã-fuji", "maçã-red-delicious", "maçã-brava",
+                           "mamão", "mamão-formosa", "mamey", "mangaba", "manga", "manga-tommy", "manga-palmer",
+                           "manga-haden", "manga-espada", "mangostão", "maracujá", "maracujá-doce", "maracujá-roxo",
+                           "melancia", "melão", "melão-gália", "melão-orange", "melão-amarelo",
+                           "mirtilo", "morango", "morango-silvestre", "murici", "murici-da-mata",
+                           "mexerica", "mexerica-rio", "mexerica-pokan",
+
+                           "nectarina",
+
+                           "olho-de-boi-fruto", "olho-de-cabra-fruto", "olho-de-macaco-fruto",
+                           "orangequat", "oranique",
+
+                           "papaia", "pêra", "pêra-william", "pêra-portuguesa", "pessego", "pêssego", "pêssego-bravo",
+                           "physalis", "pinha", "pinha-da-mata", "pimentão-fruto", "pineberry",
+                           "pitanga", "pitanga-roxa", "pitanga-branca", "pitaya", "pitaya-branca",
+                           "pitaya-vermelha", "pistache", "pixirica",
+                           "plantain (banana-da-terra)",
+
+                           "quina-do-mato-fruto",
+
+                           "rambutan", "ramphal", "raspberry", "redcurrant", "romã", "rose-apple",
+
+                           "salak (snake-fruit)", "sapoti", "sapota-branca", "sapucaia-fruto",
+                           "sapucaia-peluda", "seriguela", "soursop", "tamarillo", "tamarindo",
+                           "tâmara", "tangerina", "tangerina-cravo", "tangerina-murcote",
+                           "tangerina-montenegro", "tangerina-pokan",
+                           "tomate", "tomate-uva", "tomate-de-árvore",
+                           "toranja", "tucumã",
+
+                           "uva", "uva-verde", "uva-roxa", "uva-red-globe", "uva-sultana", "uva-moscato",
+                           "uvaia", "uxi-amarelo", "uxi-do-cerrado",
+
+                           "veludo-fruto", "veludo-da-mata", "velvet-apple",
+
+                           "wampee", "wax-apple", "wolfberry (goji)",
+
+                           "ximango-fruto",
+
+                           "yuzu", "yangmei",
+
+                           "zimbro-fruto"};
 
     int total = sizeof(lista) / sizeof(lista[0]);
 
